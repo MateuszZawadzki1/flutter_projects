8b6b8803ebb550f5fad820d38ab16387de0451b6
Mateusz Zawadzki <matizawadzki@interia.pl>
2024-10-17 14:07:16 +0200

    refactored all codes, gradle fix

---
+++ quizz_app/lib/quizz.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:quizz_app/question_screen.dart';
+import 'package:quizz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  String activeScreen = 'start-screen';
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'question-screen';
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                const Color.fromARGB(255, 60, 9, 146),
+                const Color.fromARGB(255, 92, 51, 163)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: activeScreen == 'start-screen'
+              ? StartScreen(switchScreen)
+              : QuestionScreen(),
+        ),
+      ),
+    );
+  }
+}
